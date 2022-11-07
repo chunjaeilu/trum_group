8498c3a1d63ac0ad41df1f614c12249863f4c167
KIM MINSU <112890661+chunjaeilu@users.noreply.github.com>
2022-11-08 01:29:46 +0900

    contents_3 web css 작성
    - contents_3 web css 작성
    - 버튼 이벤트 구현 (js)
    - 버튼과 뉴스컨텐츠 z-index값에 따라 클릭이 안되는 이슈, html 수정 요함

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /* main.js */
 $(function () {
+  /* contents_2 bg_img 전환 */
   let bgEl = [
     "images/contents_2_bg_1.jpg",
     "images/contents_2_bg_2.jpg",
@@ -12,5 +13,14 @@ $(function () {
       backgroundImage: `url(${bgEl[num]})`,
       transition: "1s",
     });
+  });
+
+  /* contents_3 next/prev 버튼 */
+  $(".contents_3 .btn_box .news_prev").click(function () {
+    $(".contents_3 li:last").prependTo(".contents_3 > .news_list");
+  });
+
+  $(".contents_3 .btn_box .news_next").click(function () {
+    $(".contents_3 li:first").appendTo(".contents_3 > .news_list");
   });
 });
